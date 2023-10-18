d93c87a2297a775c50800be1c5c6d64c4a7ceb85
Luka Cvijan <lcvijan@gmail.com>
2023-10-18 23:00:22 +0200

    Commiting whole project

---
+++ PP1Project/MJCompiler/src/rs/ac/bg/etf/pp1/ast/VarDecl.java
@@ -0,0 +1,96 @@
+// generated with ast extension for cup
+// version 0.8
+// 15/8/2023 0:5:28
+
+
+package rs.ac.bg.etf.pp1.ast;
+
+public class VarDecl implements SyntaxNode {
+
+    private SyntaxNode parent;
+    private int line;
+    private Type Type;
+    private IdentList IdentList;
+
+    public VarDecl (Type Type, IdentList IdentList) {
+        this.Type=Type;
+        if(Type!=null) Type.setParent(this);
+        this.IdentList=IdentList;
+        if(IdentList!=null) IdentList.setParent(this);
+    }
+
+    public Type getType() {
+        return Type;
+    }
+
+    public void setType(Type Type) {
+        this.Type=Type;
+    }
+
+    public IdentList getIdentList() {
+        return IdentList;
+    }
+
+    public void setIdentList(IdentList IdentList) {
+        this.IdentList=IdentList;
+    }
+
+    public SyntaxNode getParent() {
+        return parent;
+    }
+
+    public void setParent(SyntaxNode parent) {
+        this.parent=parent;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line=line;
+    }
+
+    public void accept(Visitor visitor) {
+        visitor.visit(this);
+    }
+
+    public void childrenAccept(Visitor visitor) {
+        if(Type!=null) Type.accept(visitor);
+        if(IdentList!=null) IdentList.accept(visitor);
+    }
+
+    public void traverseTopDown(Visitor visitor) {
+        accept(visitor);
+        if(Type!=null) Type.traverseTopDown(visitor);
+        if(IdentList!=null) IdentList.traverseTopDown(visitor);
+    }
+
+    public void traverseBottomUp(Visitor visitor) {
+        if(Type!=null) Type.traverseBottomUp(visitor);
+        if(IdentList!=null) IdentList.traverseBottomUp(visitor);
+        accept(visitor);
+    }
+
+    public String toString(String tab) {
+        StringBuffer buffer=new StringBuffer();
+        buffer.append(tab);
+        buffer.append("VarDecl(\n");
+
+        if(Type!=null)
+            buffer.append(Type.toString("  "+tab));
+        else
+            buffer.append(tab+"  null");
+        buffer.append("\n");
+
+        if(IdentList!=null)
+            buffer.append(IdentList.toString("  "+tab));
+        else
+            buffer.append(tab+"  null");
+        buffer.append("\n");
+
+        buffer.append(tab);
+        buffer.append(") [VarDecl]");
+        return buffer.toString();
+    }
+}
